02cb494eba6e2a44655abed1f8d9d88cc03e6cd8
Nicolas Martinelli <nicolas.martinelli@pm.me>
2020-05-21 09:36:11 +0200

    Make entry body and bottom bar as Stateless Widgets

--- lib/screens/entry.dart
+++ lib/screens/entry.dart
@@ -9,6 +9,14 @@ import '../models/data.dart';
 import '../models/entry.dart';
 import '../models/nav.dart';
 
+void _launchURL(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
 class MyEntryHeader extends StatelessWidget {
   MyEntryHeader({Key key, @required this.entry}) : super(key: key);
   final Entry entry;
@@ -58,15 +66,98 @@ class MyEntryHeader extends StatelessWidget {
   }
 }
 
-class MyEntry extends StatelessWidget {
-  void _launchURL(String url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
+class MyEntryBody extends StatelessWidget {
+  MyEntryBody({Key key, @required this.entry}) : super(key: key);
+  final Entry entry;
 
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            MyEntryHeader(entry: entry),
+            Html(
+              data: entry.content,
+              onLinkTap: (url) => _launchURL(url),
+              onImageTap: (url) => _launchURL(url),
+            ),
+          ],
+        ),
+      ),
+      onHorizontalDragEnd: (details) {
+        // We need to filter entries from the complete list and not from the entries available in
+        // the ListView.builder since the builder usually has a subset of entries (the ones
+        // displayed).
+        Entry nextEntry;
+        final data = Provider.of<Data>(context, listen: false);
+        final nav = Provider.of<Nav>(context, listen: false);
+        final entries = filterEntries(data, nav);
+        final index = entries.indexOf(entry);
+        if (details.velocity.pixelsPerSecond.dx < 0 &&
+            index < entries.length - 1) {
+          nextEntry = entries[index + 1];
+        } else if (details.velocity.pixelsPerSecond.dx > 0 && index > 0) {
+          nextEntry = entries[index - 1];
+        }
+        if (nextEntry != null) {
+          Navigator.pushReplacementNamed(
+            context,
+            '/entry',
+            arguments: nextEntry,
+          );
+          final List<int> entryIds = [nextEntry.id];
+          data.read(entryIds);
+        }
+      },
+    );
+  }
+}
+
+class MyEntryBottom extends StatelessWidget {
+  MyEntryBottom({Key key, @required this.entry}) : super(key: key);
+  final Entry entry;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: Row(
+        children: <Widget>[
+          Spacer(),
+          Consumer<Data>(
+            builder: (context, data, child) {
+              return IconButton(
+                icon: entry.starred
+                    ? Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      )
+                    : Icon(Icons.star_border),
+                onPressed: () => data.toggleStar(entry.id),
+              );
+            },
+          ),
+          Consumer<Data>(
+            builder: (context, data, child) {
+              return IconButton(
+                icon: Icon(entry.status == 'read'
+                    ? Icons.visibility
+                    : Icons.visibility_off),
+                onPressed: () => data.toggleRead(entry.id),
+              );
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.open_in_browser),
+            onPressed: () => _launchURL(entry.url),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyEntry extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Entry entry = ModalRoute.of(context).settings.arguments;
@@ -76,79 +167,8 @@ class MyEntry extends StatelessWidget {
           entry.feed.title,
         ),
       ),
-      body: GestureDetector(
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              MyEntryHeader(entry: entry),
-              Html(
-                data: entry.content,
-                onLinkTap: (url) => _launchURL(url),
-                onImageTap: (url) => _launchURL(url),
-              ),
-            ],
-          ),
-        ),
-        onHorizontalDragEnd: (details) {
-          // We need to filter entries from the complete list and not from the entries available in
-          // the ListView.builder since the builder usually has a subset of entries (the ones
-          // displayed).
-          Entry nextEntry;
-          final data = Provider.of<Data>(context, listen: false);
-          final nav = Provider.of<Nav>(context, listen: false);
-          final entries = filterEntries(data, nav);
-          final index = entries.indexOf(entry);
-          if (details.velocity.pixelsPerSecond.dx < 0 &&
-              index < entries.length - 1) {
-            nextEntry = entries[index + 1];
-          } else if (details.velocity.pixelsPerSecond.dx > 0 && index > 0) {
-            nextEntry = entries[index - 1];
-          }
-          if (nextEntry != null) {
-            Navigator.pushReplacementNamed(
-              context,
-              '/entry',
-              arguments: nextEntry,
-            );
-            final List<int> entryIds = [nextEntry.id];
-            data.read(entryIds);
-          }
-        },
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          children: <Widget>[
-            Spacer(),
-            Consumer<Data>(
-              builder: (context, data, child) {
-                return IconButton(
-                  icon: entry.starred
-                      ? Icon(
-                          Icons.star,
-                          color: Colors.amber,
-                        )
-                      : Icon(Icons.star_border),
-                  onPressed: () => data.toggleStar(entry.id),
-                );
-              },
-            ),
-            Consumer<Data>(
-              builder: (context, data, child) {
-                return IconButton(
-                  icon: Icon(entry.status == 'read'
-                      ? Icons.visibility
-                      : Icons.visibility_off),
-                  onPressed: () => data.toggleRead(entry.id),
-                );
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.open_in_browser),
-              onPressed: () => _launchURL(entry.url),
-            ),
-          ],
-        ),
-      ),
+      body: MyEntryBody(entry: entry),
+      bottomNavigationBar: MyEntryBottom(entry: entry),
     );
   }
 }

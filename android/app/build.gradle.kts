47b8557bfa224b0cc4a50b57cc897924342aadc9
Nicolas Martinelli <nicolas.martinelli@pm.me>
2025-06-07 15:37:10 +0200

    Bump (main) dependencies
    Upgrade dependencies to their latest version.
    Update build files from a newly created project, migrate to kts files.
    Set minSdk to 26 which corresponds to Android 8.0 (August 2017).
    Remove ios files.
    
    Flutter doctor:
     - Flutter version 3.32.2 on channel stable
     - Framework revision 8defaa71a7
     - Engine revision 1091508939
     - Dart version 3.8.1
     - DevTools version 2.45.1
     - Platform android-36, build-tools 36.0.0-rc1
     - Java version OpenJDK Runtime Environment
       (build 17.0.10+0-17.0.10b1087.21-11572160)

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,70 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+android {
+    namespace = "be.martinelli.miniflutt"
+    compileSdk = flutter.compileSdkVersion
+    // ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    //sourceSets {
+    //    getByName("main").java.srcDirs("src/main/kotlin")
+    //}
+
+    defaultConfig {
+        applicationId = "be.martinelli.miniflutt"
+        minSdk = 26
+        targetSdk = flutter.targetSdkVersion
+        versionCode = 1
+        versionName = "1.15.0"
+    }
+
+    signingConfigs {
+        create("release") {
+            if (keystorePropertiesFile.exists()) {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = keystoreProperties["storeFile"]?.let { file(it.toString()) }
+                storePassword = keystoreProperties["storePassword"] as String
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+            ndk {
+                debugSymbolLevel = "SYMBOL_TABLE"
+            }
+        }
+        debug {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+} 
